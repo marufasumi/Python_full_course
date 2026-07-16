75972c4c39357cec1b177727fc0060201600b424
Marufa Sultana Sumi <110339607+marufasumi@users.noreply.github.com>
2026-07-16 09:48:31 -0400

    Add type checks for variables in data_types.py
    Added print statements to check data types of variables.

--- 1. Variables and Data Types/data_types.py
+++ 1. Variables and Data Types/data_types.py
@@ -3,7 +3,11 @@ age=25.5   # integer
 gender='Male'  # string can be in double quote or single quote
 is_student=True  # Boolean
 
+
 print(type(name))
 print(type(age))
 print(type(is_student))
 print(type(gender))
+
+
+# To check the data type use type() function
