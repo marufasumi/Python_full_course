33ff0141afabae8d73855b100338e1615988c3ab
Marufa Sultana Sumi <110339607+marufasumi@users.noreply.github.com>
2026-07-16 09:49:14 -0400

    Update data_types.py

--- 1. Variables and Data Types/data_types.py
+++ 1. Variables and Data Types/data_types.py
@@ -3,7 +3,7 @@ age=25.5   # integer
 gender='Male'  # string can be in double quote or single quote
 is_student=True  # Boolean
 
-
+# To check data type we should use type() function
 print(type(name))
 print(type(age))
 print(type(is_student))
